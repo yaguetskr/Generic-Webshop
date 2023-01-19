e95d5b3bf5421e6f939551a318717e39cd9035b9
yaguetskr <60235122+yaguetskr@users.noreply.github.com>
2023-01-19 17:52:55 +0100

    CRUD

--- frontend/src/main/java/com/rest/frontend/API.java
+++ frontend/src/main/java/com/rest/frontend/API.java
@@ -21,5 +21,41 @@ public class API {
 
     }
 
+    public String get(int id) throws URISyntaxException, IOException, InterruptedException {
+
+        String fullUrl=String.format(backendUrlPrefix,"get?id="+Integer.toString(id));
+        HttpRequest request= HttpRequest.newBuilder().uri(new URI(fullUrl)).GET().build();
+        HttpResponse<String> response = HttpClient.newBuilder().build().send(request,HttpResponse.BodyHandlers.ofString());
+        return response.body();
+
+    }
+
+    public void edit(int id,int stock,float price, String name) throws URISyntaxException, IOException, InterruptedException {
+
+        String fullUrl=String.format(backendUrlPrefix,"edit?id="+Integer.toString(id)+"&stock="+Integer.toString(stock)+"&price="+Float.toString(price)+"&name="+name);
+        HttpRequest request= HttpRequest.newBuilder().uri(new URI(fullUrl)).GET().build();
+        HttpResponse<String> response = HttpClient.newBuilder().build().send(request,HttpResponse.BodyHandlers.ofString());
+
+
+    }
+
+    public void create(int stock,float price, String name) throws URISyntaxException, IOException, InterruptedException {
+
+        String fullUrl=String.format(backendUrlPrefix,"create?stock="+Integer.toString(stock)+"&price="+Float.toString(price)+"&name="+name);
+        HttpRequest request= HttpRequest.newBuilder().uri(new URI(fullUrl)).GET().build();
+        HttpResponse<String> response = HttpClient.newBuilder().build().send(request,HttpResponse.BodyHandlers.ofString());
+
+
+    }
+
+    public void delete(int id) throws URISyntaxException, IOException, InterruptedException {
+
+        String fullUrl=String.format(backendUrlPrefix,"delete?id="+Integer.toString(id));
+        HttpRequest request= HttpRequest.newBuilder().uri(new URI(fullUrl)).GET().build();
+        HttpResponse<String> response = HttpClient.newBuilder().build().send(request,HttpResponse.BodyHandlers.ofString());
+
+
+    }
+
 
 }
